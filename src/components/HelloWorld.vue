c97e5e037f4ec33020558b3b80e293376b81d124
marmuze <38520142+marmuze@users.noreply.github.com>
2018-10-08 09:56:47 +0200

    test branch

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -2,7 +2,7 @@
   <div class="hello">
     <h1>{{ msg }}</h1>
     <p>
-     tous sur la configuration
+     tous sur la configuration ou presque
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
     </p>
     <h3>Installed CLI Plugins</h3>
